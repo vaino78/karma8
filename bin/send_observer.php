5ae574fa1e63d9463622040ae8628a333166b3cb
Ivan Danilov <vaino78@yandex.ru>
2023-09-03 21:45:45 +0300

    Adding struct for sending observer

---
+++ bin/send_observer.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../shared/bootstrap.php';
+
+$db = include APP_SHARED_PATH . '/db.php';
+
+$checkTimestamp = env_extract_send_observer_timestamp($argv);
+$limit = env_extract_send_observer_limit();
+$pause = env_extract_send_observer_pause();
+
+while (true) {
+  $observed = send_observer_process_pack($db, $checkTimestamp, $limit);
+  if ($observed === 0) {
+    if ($pause > 0) {
+      sleep($pause);
+    }
+    continue;
+  }
+}
